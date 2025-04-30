d6046c18ada9f0768111948d0b5604669e13ae64
Ashish Kumar Sahoo <ashishsahoo2015@gmail.com>
2025-04-30 13:28:59 +0530

    Upated TransactionActivityChart file

--- pages/components/TransactionActivityChart.js
+++ pages/components/TransactionActivityChart.js
@@ -1,6 +1,14 @@
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid,
+} from "recharts";
 
-export default function TransactionActivityChart ({ data }) {
+export default function TransactionActivityChart({ data }) {
   return (
     <section className="mt-12">
       <h2 className="text-2xl font-semibold text-blue-700 mb-4">
@@ -8,18 +16,20 @@ export default function TransactionActivityChart ({ data }) {
       </h2>
       <div className="w-full h-64 bg-white rounded-lg shadow-md p-4">
         <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            data={data.slice(-30)}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="date" tick={{ fontSize: 10 }} />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="count" fill="#4299E1" radius={[4, 4, 0, 0]} />
-          </BarChart>
+          {data?.length > 0 ? (
+            <BarChart
+              data={data?.slice(-30)}
+              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" tick={{ fontSize: 10 }} />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="count" fill="#4299E1" radius={[4, 4, 0, 0]} />
+            </BarChart>
+          ) : null}
         </ResponsiveContainer>
       </div>
     </section>
   );
-};
+}
